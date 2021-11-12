ec707459ac08b3c9521514cefe447e349abd92a3
Andrew Davis <asutherlandavis@googlemail.com>
2021-11-12 11:19:02 +0000

    Traffic Light code

--- GameFest/Assets/CoinCompletionController.cs
+++ GameFest/Assets/CoinCompletionController.cs
@@ -5,13 +5,32 @@ public class CoinCompletionController : MonoBehaviour
 {
     public CoinScript[] Coins;
     public PlatformBase[] PlatformsToEnable;
+    public SpriteRenderer TrafficLight;
+    public Sprite[] TrafficLightSprites;
 
+    enum TrafficLightSpriteIndexes { Red, Amber, Green };
+
+    /// <summary>
+    /// Checks if all the coins have been collected
+    /// </summary>
     public void CheckCompletion()
     {
-        if(!Coins.Any(c => c.IsActive()) || !Coins.Any(c => c.gameObject.activeInHierarchy))
+        var count = Coins.Where(c => c.IsActive());
+    
+        // if all coins complete, enable the platforms and update traffic light display
+        if(count == Coins.Length)
         {
+            // traffic light goes to green
+            TrafficLight.sprite = TrafficLightSprites[TrafficLightSpriteIndexes.Green];
+        
+            // platforms
             foreach (var p in PlatformsToEnable)
                 p.Enabled(true);
+        }
+        // more than half have been collected, so traffic light goes to amber
+        else if (count > (Coins.Length / 2))
+        {        
+            TrafficLight.sprite = TrafficLightSprites[TrafficLightSpriteIndexes.Amber];
         }
     }
 }
