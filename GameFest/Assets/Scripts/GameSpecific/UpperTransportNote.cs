d3b7e61bf38000ebdec6e32f6eb0cf71be7ec345
Andrew Davis <asutherlandavis@googlemail.com>
2021-11-12 11:25:59 +0000

    Script for note movement in the upper transport

---
+++ GameFest/Assets/Scripts/GameSpecific/UpperTransportNote.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Controls the movement of a note through the top transport
+/// </summary>
+public class UpperTransportNote /* : MonoBehaviour */
+{
+    float _noteResetX;
+    bool _moving = false;
+
+    const float SPEED = 2.5f;
+
+    /// <summary>
+    /// Starts the movement of the note to the specified point
+    /// </summary>
+    /// <param name="noteStartX">Where the note starts</param>
+    /// <param name="noteResetX">Where to stop the movement</param>
+    public void StartMovement(float noteStartX, float noteResetX)
+    {
+        transform.localPosition = new Vector3(noteStartX, transform.localPosition.y, transform.localPosition.z);
+        _moving = true;
+    }
+
+    void Update()
+    {
+        if (_moving)
+        {
+            //transform.Translate(new Vector3(SPEED * Time.deltaTime, 0, 0));
+            if (transform.localPosition.x < _noteResetX)
+            {
+                _moving = false;
+            }
+        }
+    }
+}
