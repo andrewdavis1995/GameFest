676e8b3e6b8404475683e0d45ca6e71e7d5af303
Andrew Davis <asutherlandavis@googlemail.com>
2021-11-23 15:15:03 +0000

    Hide unused elements

--- GameFest/Assets/Scripts/GameControllers/CartAttackController.cs
+++ GameFest/Assets/Scripts/GameControllers/CartAttackController.cs
@@ -19,17 +19,36 @@ public class CartAttackController : MonoBehaviour
         
         // TODO: this moves to SpawnPlayers (add to list)
         _players = FindObjectsOfType<CartAttackInputHandler>().ToList();
-        for(int index = 0; index < Cars.Length && index < _players.Count; index++)
+        int index = 0;
+        for(; index < Cars.Length && index < _players.Count; index++)
         {
             // assign car to player
             player.SetCarController(Cars[index]);
         }
+        
+        // TODO: Move to after SpawnPlayers
+        HideUnusedElements_(index, Cars.Length);
         
         // TODO: this moves to after the countdown lights
         // enable all players
         foreach(var player in _players)
         {
             player.SetActiveState(true);
+        }
+    }
+    
+    /// <summary>
+    /// Hides cars and UI elements that are not needed (due to there not being the full 4 players playing)
+    /// </summary>
+    /// <param id="index">The index to start at</param>
+    /// <param id="index">The maximum number of items to go up to</param>
+    void HideUnusedElements_(int index, int maximum)
+    {
+        // hide unused cars
+        for(; index < maximum; index++)
+        {
+            // hide car
+            Cars[index].gameObject.SetActive(false);
         }
     }
 }
