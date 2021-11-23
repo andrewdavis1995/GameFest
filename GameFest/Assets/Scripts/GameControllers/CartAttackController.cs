bd9c78000c0fd3d30b16e2e3431b2a09d381d709
Andrew Davis <asutherlandavis@googlemail.com>
2021-11-23 14:21:00 +0000

    Enable all players at start
    Assign cars to player through controller

--- GameFest/Assets/Scripts/GameControllers/CartAttackController.cs
+++ GameFest/Assets/Scripts/GameControllers/CartAttackController.cs
@@ -1,13 +1,35 @@
 using UnityEngine;
 
+/// <summary>
+/// Controls the flow of "Cart Attack"
+/// </summary>
 public class CartAttackController : MonoBehaviour
 {
     public Collider2D[] Checkpoints;
+    public CarControllerScript[] Cars;
+    
+    List<CartAttackInputHandler> _players = new List<CartAttackInputHandler>();
 
     public static CartAttackController Instance;
 
+    // Called once on startup
     private void Start()
     {
         Instance = this;
+        
+        // TODO: this moves to SpawnPlayers (add to list)
+        _players = FindObjectsOfType<CartAttackInputHandler>().ToList();
+        for(int index = 0; index < Cars.Length && index < _players.Count; index++)
+        {
+            // assign car to player
+            player.SetCarController(Cars[index]);
+        }
+        
+        // TODO: this moves to after the countdown lights
+        // enable all players
+        foreach(var player in _players)
+        {
+            player.SetActiveState(true);
+        }
     }
 }
