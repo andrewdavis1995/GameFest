37a1e420aa055c385e33b58fb15b6404bcedc837
Andrew Davis <asutherlandavis@googlemail.com>
2021-11-23 14:14:11 +0000

    Only allow car movement during race
    Also refactored InputHandler (so far)

--- GameFest/Assets/Scripts/InputHandlers/MiniGameInputHandlers/CartAttackInputHandler.cs
+++ GameFest/Assets/Scripts/InputHandlers/MiniGameInputHandlers/CartAttackInputHandler.cs
@@ -1,12 +1,39 @@
 using UnityEngine;
 
+/// <summary>
+/// Input handler for Cart Attack mini-game
+/// </summary>
 public class CartAttackInputHandler : MonoBehaviour
 {
     CarControllerScript _carController;
+    
+    bool _active = false;
 
+    /// <summary>
+    /// Called once when the script begins
+    /// </summary>
     private void Awake()
     {
+        // TODO: remove once hooked up with proper controller and game system
         _carController = GetComponent<CarControllerScript>();
+    }
+    
+    /// <summary>
+    /// Sets whether the car can move
+    /// </summary>
+    /// <param id="state">Whether the car can move</param>
+    public void SetActiveState(bool state)
+    {
+        _active = state;
+    }
+    
+    /// <summary>
+    /// Sets the car to use for this player
+    /// </summary>
+    /// <param id="car">The car to use</param>
+    public void SetCarController(CarController car)
+    {
+        _carController = car;
     }
 
     // Update is called once per frame
@@ -24,10 +51,14 @@ public class CartAttackInputHandler : MonoBehaviour
         if (Input.GetKey(KeyCode.S))
             y = -1;
 
-        if (Input.GetKey(KeyCode.Space))
-            _carController.Boost();
+        // TODO: Move to inputsystem controls
+        if(_active)
+        {
+            if (Input.GetKey(KeyCode.Space))
+                _carController.Boost();
 
-        _carController.SetAccelerationValue(y);
-        _carController.SetSteeringValue(x);
+            _carController.SetAccelerationValue(y);
+            _carController.SetSteeringValue(x);
+        }
     }
 }
