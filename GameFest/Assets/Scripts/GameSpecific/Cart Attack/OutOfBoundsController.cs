abc5287992a935a35b30d0c714c498d004a1fa04
Andrew Davis <asutherlandavis@googlemail.com>
2021-11-25 12:23:52 +0000

    Update OutOfBoundsController.cs

--- GameFest/Assets/Scripts/GameSpecific/Cart Attack/OutOfBoundsController.cs
+++ GameFest/Assets/Scripts/GameSpecific/Cart Attack/OutOfBoundsController.cs
@@ -1,23 +1,37 @@
 using UnityEngine;
 
+/// <summary>
+/// Controller for back off car, to check if it went out of bounds
+/// </summary>
 public class OutOfBoundsController : MonoBehaviour
 {
     public CarControllerScript Car;
 
+    /// <summary>
+    /// Called when something enters the trigger
+    /// <summary>
+    /// <param id="collision">The item that entered the trigger</param>
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // checkpoints/finish line
         if (collision.gameObject.tag == "Finish" || collision.gameObject.tag == "Checkpoint")
         {
             Car.NextCheckpoint(collision);
         }
+        // out of bounds
         else if (collision.gameObject.tag == "AreaTrigger")
         {
             Car.OutOfBoundsEntered(collision);
         }
     }
 
+    /// <summary>
+    /// Called when something leaves the trigger
+    /// <summary>
+    /// <param id="collision">The item that left the trigger</param>
     private void OnTriggerExit2D(Collider2D collision)
     {
+        // out of bounds
         if (collision.gameObject.tag == "AreaTrigger")
         {
             Car.OutOfBoundsExited(collision);
