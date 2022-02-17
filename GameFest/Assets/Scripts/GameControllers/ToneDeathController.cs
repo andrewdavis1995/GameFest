f75eebab6c1fcaa06109cd50f0ef7255523ab960
Andrew Davis <asutherlandavis@googlemail.com>
2022-02-17 11:58:15 +0000

    Timers set up
    Need to control when they start/abort now

--- GameFest/Assets/Scripts/GameControllers/ToneDeathController.cs
+++ GameFest/Assets/Scripts/GameControllers/ToneDeathController.cs
@@ -10,6 +10,8 @@ public class ToneDeathController : GenericController
     float FLOOR_HEIGHT = 10f;
     uint FLOOR_COUNT = 2;
     const float ELEVATOR_SPEED = 0.25f;
+    const int OVERALL_LEVEL_TIMEOUT = 120;
+    const int ELEVATOR_TIMEOUT = 30;
 
     public static ToneDeathController Instance;
 
@@ -22,10 +24,20 @@ public class ToneDeathController : GenericController
     List<ToneDeathInputHandler> _players;
     int _elevatorIndex = 0;
 
+    // timers
+    TimeLimit _levelTimer;
+    TimeLimit _elevatorEndTimer;    
+    
     // Start is called before the first frame update
     void Start()
     {
         Instance = this;
+
+        // initialise timers        
+        _levelTimer = (TimeLimit)gameObject.AddComponent(typeof(TimeLimit));
+        _levelTimer.Initialise(OVERALL_LEVEL_TIMEOUT, levelTimerTick_, levelTimerComplete_, 1f);
+        _elevatorEndTimer = (TimeLimit)gameObject.AddComponent(typeof(TimeLimit));
+        _elevatorEndTimer.Initialise(ELEVATOR_TIMEOUT, elevatorTimerTick_, elevatorTimerComplete_, 1f);
 
         // get the distance between floors
         FLOOR_HEIGHT = Elevators[1].transform.position.y - Elevators[0].transform.position.y;
@@ -41,6 +53,56 @@ public class ToneDeathController : GenericController
         // TODO
         //SpawnPlayers_();
     }
+    
+    /// <summary>
+    /// Callback for when the timer ticks
+    /// </summary>
+    private void levelTimerTick_(int time)
+    {
+        // TODO: show remaining time once it get below 20 seconds or so
+    }
+    
+    /// <summary>
+    /// Callback for when the timer completes
+    /// </summary>
+    private void levelTimerComplete_()
+    {
+        KillIncompletePlayers_();
+    }
+    
+    /// <summary>
+    /// Kills any players who are not already complete
+    /// </summary>
+    void KillIncompletePlayers_()
+    {    
+        // mark all players as complete and close elevator
+        foreach(var p in _players)
+        {
+            if(!p.FloorComplete())
+            {
+                // if the player did not make it to the escalator, they are dead
+                p.DamageDone(1000f);
+            }
+        }    
+        
+        StartCoroutine(NextLevel_());
+    }
+    
+    /// <summary>
+    /// Callback for when the timer ticks
+    /// </summary>
+    private void elevatorTimerTick_(int time)
+    {
+        // TODO: show remaining time (above door)
+    }
+    
+    /// <summary>
+    /// Callback for when the timer completes
+    /// </summary>
+    private void elevatorTimerComplete_()
+    {
+        KillIncompletePlayers_();
+    }
 
     /// <summary>
     /// Checks if all players are complete, and moves to the next level if applicable
@@ -48,14 +110,9 @@ public class ToneDeathController : GenericController
     public void CheckAllPlayersComplete()
     {
         // if all done, move elevator
-        if (_players.All(p => p.FloorComplete()) && _players.Any(p => !p.Died()))
+        if (_players.All(p => p.FloorComplete() || p.Died()))
         {
             StartCoroutine(NextLevel_());
-        }
-        else if(_players.All(p => p.Died()))
-        {
-            // if all dead, end the game
-            EndGame_();
         }
     }
 
@@ -64,60 +121,68 @@ public class ToneDeathController : GenericController
     /// </summary>
     private IEnumerator NextLevel_()
     {
-        // close doors
-        for (var i = 0; i < ElevatorDoors.Count(); i++)
+        // if everyone dead, end the game
+        if(_players.All(p => p.Died())
         {
-            // loop through images
-            Elevators[_elevatorIndex].Doors.sprite = ElevatorDoors[i];
-            yield return new WaitForSeconds(.05f);
+            EndGame_();
         }
-
-        // hide all players (before moving up)
-        foreach (var v in _players)
+        else
         {
-            v.Hide();
-        }
-
-        // move elevator and camera up
-        for (float i = 0; i < FLOOR_HEIGHT; i += ELEVATOR_SPEED)
-        {
-            Camera.main.transform.Translate(new Vector3(0, ELEVATOR_SPEED, 0));
-            Elevators[_elevatorIndex].Platform.Translate(new Vector3(0, ELEVATOR_SPEED, 0));
-            yield return new WaitForSeconds(.01f);
-        }
-
-        // show all players (behind doors)
-        foreach (var v in _players)
-        {
-            v.Show();
-        }
-
-        // extra bit so they are above the floor
-        Elevators[_elevatorIndex].Platform.Translate(new Vector3(0, ELEVATOR_SPEED, 0));
-
-        // open the top doors
-        if (Elevators[_elevatorIndex].Top != null)
-        {
-            // loop through each image
-            for (var i = ElevatorDoors.Count() - 1; i >= 0; i--)
+            // close doors
+            for (var i = 0; i < ElevatorDoors.Count(); i++)
             {
-                Elevators[_elevatorIndex].Top.Doors.sprite = ElevatorDoors[i];
+                // loop through images
+                Elevators[_elevatorIndex].Doors.sprite = ElevatorDoors[i];
                 yield return new WaitForSeconds(.05f);
             }
+
+            // hide all players (before moving up)
+            foreach (var v in _players)
+            {
+                v.Hide();
+            }
+
+            // move elevator and camera up
+            for (float i = 0; i < FLOOR_HEIGHT; i += ELEVATOR_SPEED)
+            {
+                Camera.main.transform.Translate(new Vector3(0, ELEVATOR_SPEED, 0));
+                Elevators[_elevatorIndex].Platform.Translate(new Vector3(0, ELEVATOR_SPEED, 0));
+                yield return new WaitForSeconds(.01f);
+            }
+
+            // show all players (behind doors)
+            foreach (var v in _players)
+            {
+                v.Show();
+            }
+
+            // extra bit so they are above the floor
+            Elevators[_elevatorIndex].Platform.Translate(new Vector3(0, ELEVATOR_SPEED, 0));
+
+            // open the top doors
+            if (Elevators[_elevatorIndex].Top != null)
+            {
+                // loop through each image
+                for (var i = ElevatorDoors.Count() - 1; i >= 0; i--)
+                {
+                    Elevators[_elevatorIndex].Top.Doors.sprite = ElevatorDoors[i];
+                    yield return new WaitForSeconds(.05f);
+                }
+            }
+
+            // make players leave elevator
+            foreach (var v in _players)
+            {
+                v.PlayerExitElevator();
+            }
+
+            // next floor
+            _elevatorIndex++;
+
+            // end game if this was the last level
+            if (_elevatorIndex >= FLOOR_COUNT)
+                EndGame_();
         }
-
-        // make players leave elevator
-        foreach (var v in _players)
-        {
-            v.PlayerExitElevator();
-        }
-
-        // next floor
-        _elevatorIndex++;
-
-        // end game
-        if (_elevatorIndex >= FLOOR_COUNT)
-            EndGame_();
     }
 
     /// <summary>
