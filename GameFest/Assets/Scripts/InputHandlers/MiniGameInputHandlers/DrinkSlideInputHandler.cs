76ebf9785487dfbdaf7eecba9bd121852c551456
Andrew Davis <asutherlandavis@googlemail.com>
2022-03-04 12:32:00 +0000

    Angle calculation

--- GameFest/Assets/Scripts/InputHandlers/MiniGameInputHandlers/DrinkSlideInputHandler.cs
+++ GameFest/Assets/Scripts/InputHandlers/MiniGameInputHandlers/DrinkSlideInputHandler.cs
@@ -30,16 +30,18 @@ public class DrinkSlideInputHandler : GenericInputHandler
         if (_joystickReadings.Count > NUM_READINGS)
             _joystickReadings.RemoveAt(0);
 
+        // calculate angle - flip by -1 to point opposite direction
+        float angle = Mathf.Atan2(_joystickReadings[0].y _joystickReadings[0].x) * 180 * -1 * Mathf.PI - 90;
+        if(_isActive)
+            DrinkSlideController.Instance.UpdatePointer(GetPlayerIndex(), angle);
+                
         if (ctx.ReadValue<Vector2>().y >= -0.05f && _joystickReadings.Count == NUM_READINGS)
         {
             if(_canFire)
             {   
                 _canFire = false;
                 _isActive = false;
-                Debug.Log("IT WAS: " + _joystickReadings[0].y);
-                // TODO: calculate angle
-                float angle = 0f;
-                // TODO: calculate power
+                // calculate power
                 float powerMultiplier = Math.Abs(_joystickReadings[0].x) + Math.Abs(_joystickReadings[0].y);
                 // pass to controller (with power and angle)
                 DrinkSlideController.Instance.Fire(GetPlayerIndex(), angle, powerMultiplier);
@@ -47,7 +49,7 @@ public class DrinkSlideInputHandler : GenericInputHandler
         }
     }
 
-    public override void OnMoveRight(InputAction.CallbackContext ctx, InputDevice device)
+    public override void OnMoveRight(InputAction.CallbackContext ctx)
     {        
         _spin = ctx.ReadValue<Vector2>();
     }
