aae0d4097c2b06a77213e5b25b9926355d271b10
Andrew Davis <asutherlandavis@googlemail.com>
2022-03-04 13:24:18 +0000

    Create GlassShardScript.cs

---
+++ GameFest/Assets/Scripts/GameSpecific/GlassShardScript.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// Script for the glass shards in Drink Slide
+/// </summary>
+public class GlassShardScript : MonoBehaviour
+{
+    public SpriteRenderer Renderer;
+
+    public void Create()
+    {
+        Renderer.sprite = DrinkSlideController.Instance.GetRandomGlassShardSprite();
+        transform.Translate(new Vector2(UnityEngine.Random.Range(-0.5f, 0.5f), UnityEngine.Random.Range(-0.5f, 0.5f)));
+        StartCoroutine(SpawnOut_());
+    }
+    
+    IEnumerator SpawnOut_()
+    {
+        var a = 1f;
+        while(a > 0)
+        {
+            a -= 0.01f;
+            Renderer.color = new Color(1f, 1f, 1f, a);
+            yield return new WaitForSeconds(0.1f);
+        }
+        
+        // all gone
+        Destroy(gameObject);
+    }
+}
